3652e08f6eecac284fd539ac4faba9f03ae92f0b
Mary <miluskasaire512@gmail.com>
2024-05-11 10:58:23 -0500

    Actu

---
+++ src/main/java/co/edu/uniquindio/storify/enums/TipoArtista.java
@@ -0,0 +1,5 @@
+package co.edu.uniquindio.storify.enums;
+
+public enum TipoArtista {
+    SOLISTA, BANDA
+}
---
+++ src/main/java/co/edu/uniquindio/storify/enums/TipoGenero.java
@@ -0,0 +1,5 @@
+package co.edu.uniquindio.storify.enums;
+
+public enum TipoGenero {
+    ROCK, POP, SALSA, BACHATA, PUNK, REGGAETON, ELECTRONICA
+}
